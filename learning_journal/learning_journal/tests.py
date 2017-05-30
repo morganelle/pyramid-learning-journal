f6207d73510c7dc05e3ebe76b3af8556c073cb4e
James <jjfeore>
2017-05-30 15:56:29 -0700

    All tests added and passing

--- learning_journal/learning_journal/tests.py
+++ learning_journal/learning_journal/tests.py
@@ -1,65 +1,103 @@
-import unittest
-import transaction
-
 from pyramid import testing
+from pyramid.response import Response
+import pytest
 
 
-def dummy_request(dbsession):
-    return testing.DummyRequest(dbsession=dbsession)
+@pytest.fixture
+def list_response():
+    """Return a response from the list view."""
+    from learning_journal.views.default import list_view
+    request = testing.DummyRequest()
+    response = list_view(request)
+    return response
 
 
-class BaseTest(unittest.TestCase):
-    def setUp(self):
-        self.config = testing.setUp(settings={
-            'sqlalchemy.url': 'sqlite:///:memory:'
-        })
-        self.config.include('.models')
-        settings = self.config.get_settings()
-
-        from .models import (
-            get_engine,
-            get_session_factory,
-            get_tm_session,
-            )
-
-        self.engine = get_engine(settings)
-        session_factory = get_session_factory(self.engine)
-
-        self.session = get_tm_session(session_factory, transaction.manager)
-
-    def init_database(self):
-        from .models.meta import Base
-        Base.metadata.create_all(self.engine)
-
-    def tearDown(self):
-        from .models.meta import Base
-
-        testing.tearDown()
-        transaction.abort()
-        Base.metadata.drop_all(self.engine)
+def test_list_view_returns_response_given_request(list_response):
+    """List view returns a Response object when given a request."""
+    assert isinstance(list_response, Response)
 
 
-class TestMyViewSuccessCondition(BaseTest):
-
-    def setUp(self):
-        super(TestMyViewSuccessCondition, self).setUp()
-        self.init_database()
-
-        from .models import MyModel
-
-        model = MyModel(name='one', value=55)
-        self.session.add(model)
-
-    def test_passing_view(self):
-        from .views.default import my_view
-        info = my_view(dummy_request(self.session))
-        self.assertEqual(info['one'].name, 'one')
-        self.assertEqual(info['project'], 'Learning Journal')
+def test_list_view_is_good(list_response):
+    """List view response comes with a status 200 OK."""
+    assert list_response.status_code == 200
 
 
-class TestMyViewFailureCondition(BaseTest):
+def test_list_view_returns_proper_content(list_response):
+    """List view response includes the content we added."""
+    expected_text = '<p>Morgan Nomura // Blog</p>'
+    assert expected_text in list_response.text
 
-    def test_failing_view(self):
-        from .views.default import my_view
-        info = my_view(dummy_request(self.session))
-        self.assertEqual(info.status_int, 500)
+
+@pytest.fixture
+def detail_response():
+    """Return a response from the detail view."""
+    from learning_journal.views.default import detail_view
+    request = testing.DummyRequest()
+    response = detail_view(request)
+    return response
+
+
+def test_detail_view_returns_response_given_request(detail_response):
+    """Detail view returns a Response object when given a request."""
+    assert isinstance(detail_response, Response)
+
+
+def test_detail_view_is_good(detail_response):
+    """List detail response comes with a status 200 OK."""
+    assert detail_response.status_code == 200
+
+
+def test_detail_view_returns_proper_content(detail_response):
+    """Detail view response includes the content we added."""
+    expected_text = '<p class="post-content">Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+    assert expected_text in detail_response.text
+
+
+@pytest.fixture
+def create_response():
+    """Return a response from the create view."""
+    from learning_journal.views.default import create_view
+    request = testing.DummyRequest()
+    response = create_view(request)
+    return response
+
+
+def test_create_view_returns_response_given_request(create_response):
+    """Create view returns a Response object when given a request."""
+    assert isinstance(create_response, Response)
+
+
+def test_create_view_is_good(create_response):
+    """List create response comes with a status 200 OK."""
+    assert create_response.status_code == 200
+
+
+def test_create_view_returns_proper_content(create_response):
+    """Create view response includes the content we added."""
+    expected_text = '<h1>Create a new post</h1>'
+    assert expected_text in create_response.text
+
+
+@pytest.fixture
+def update_response():
+    """Return a response from the update view."""
+    from learning_journal.views.default import update_view
+    request = testing.DummyRequest()
+    response = update_view(request)
+    return response
+
+
+def test_update_view_returns_response_given_request(update_response):
+    """Update view returns a Response object when given a request."""
+    assert isinstance(update_response, Response)
+
+
+def test_update_view_is_good(update_response):
+    """List update response comes with a status 200 OK."""
+    assert update_response.status_code == 200
+
+
+def test_update_view_returns_proper_content(update_response):
+    """Update view response includes the content we added."""
+    expected_text = '<h1>Edit post</h1>'
+    assert expected_text in update_response.text
