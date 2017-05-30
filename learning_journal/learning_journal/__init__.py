b6e14fe507905f8b6c1e006cb21b3ec970333f8b
James <jjfeore>
2017-05-30 15:08:01 -0700

    Got the routes and view working

--- learning_journal/learning_journal/__init__.py
+++ learning_journal/learning_journal/__init__.py
@@ -2,11 +2,11 @@ from pyramid.config import Configurator
 
 
 def main(global_config, **settings):
-    """ This function returns a Pyramid WSGI application.
-    """
+    """This function returns a Pyramid WSGI application."""
     config = Configurator(settings=settings)
     config.include('pyramid_jinja2')
-    config.include('.models')
+    # config.include('.models')
+    config.include('.views')
     config.include('.routes')
     config.scan()
     return config.make_wsgi_app()
