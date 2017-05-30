dd7b47f78bae548a758557961e99a0f8c0a1533c
Morgan Nomura <morganelle@gmail.com>
2017-05-29 21:21:22 -0700

    removed scripts

--- morgan-mockups/scripts/controllers/routes.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-page('/journal', journalController.display);
-page('/journal/:id', journalController.displayByCategory);
-page('/projects', projectsController.display);
-page('/', homeController.display);
-page();
